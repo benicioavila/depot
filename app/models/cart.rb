27878256e1b4501f7068323c746971baf7cd76ab
Benicio Avila <benicio.avila@gmail.com>
2012-07-26 14:24:36 -0300

    Criando o laytou da página index e alguns testes funcionais

---
+++ app/models/cart.rb
@@ -0,0 +1,14 @@
+class Cart < ActiveRecord::Base
+    protect_from_forgery
+
+    private
+
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id]=cart.id
+      cart
+    end
+
+end
